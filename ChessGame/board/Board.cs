a4dab8bdb1375bd270e5f4c49295f2a80232731f
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-07-24 12:46:41 -0300

    Board added

---
+++ ChessGame/board/Board.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChessGame.board
+{
+    class Board
+    {
+    }
+}
---
+++ ChessGame/board/Piece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get;protected set; }
+        public int MoveQuantities { get;protected set; }
+        public oard 
+    }
+}
