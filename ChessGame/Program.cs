e1c9a195193b456eb9389826f40ae3449794c910
Wilhelm <wilhelmzimmermann8@gmail.com>
2021-07-24 12:46:23 -0300

    New Class added Position

---
+++ ChessGame/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using board;
+
+namespace ChessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Position p = new Position(1, 2);
+
+            Console.WriteLine(p);
+        }
+    }
+}
